6e6423b469a971db9defed56b25e4dd0bdcff15d
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-11 16:23:32 +0530

    Create ques3.cpp

---
+++ ques3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+    int count=0;
+    getline(cin,name);
+    for(int i=0;i<name.length();i++)
+    {
+        if(name[i]==' ')
+        {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
